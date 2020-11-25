ff4b1bc580144fd0698332ed6e2ffa3fb5ae5dee
dianatarnu <64360290+dianatarnu@users.noreply.github.com>
2020-11-26 00:31:47 +0200

    Add site

---
+++ js.js
@@ -0,0 +1,11 @@
+function goToStiri(){
+    location.replace("html.html")
+}
+
+function goToArhiva(){
+    location.replace("arhiva.html")
+}
+
+function goToIstoric(){
+    location.replace("istoric.html")
+}
